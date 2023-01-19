7a6a5ef7db85587a351508a450dc5c47a63effcf
Harry Crane <37228043+hwcrane@users.noreply.github.com>
2023-01-19 14:12:47 +0000

    initial commit

---
+++ lua/config/settings.lua
@@ -0,0 +1,29 @@
+local set = vim.opt
+
+set.nu = true
+set.relativenumber = true
+
+set.tabstop = 4
+set.softtabstop = 4
+set.shiftwidth = 4
+set.expandtab = true
+
+set.smartindent = true
+
+set.wrap = false
+
+set.swapfile = false
+set.backup = false
+set.undodir = os.getenv("HOME") .. "/.vim/undodir"
+set.undofile = true
+
+set.hlsearch = false
+set.incsearch = true
+
+set.termguicolors = true
+
+set.scrolloff = 8
+set.signcolumn = "yes"
+
+set.updatetime = 50
+
