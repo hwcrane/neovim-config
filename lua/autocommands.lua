1d18142ee060b12fc790a4fd28a373fee2c3e969
Harry Crane <37228043+hwcrane@users.noreply.github.com>
2025-03-06 10:00:10 +0000

    more changes

--- lua/autocommands.lua
+++ lua/autocommands.lua
@@ -1,8 +1,9 @@
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = {"*.typ", "*.md"},
+  pattern = {"typst", "markdown"},
   callback = function ()
     vim.opt_local.wrap = true
     vim.opt_local.linebreak = true
     vim.opt_local.spell = true
+    vim.opt_local.breakindent = true
   end,
 })
--- lua/plugins/_conform.lua
+++ lua/plugins/_conform.lua
@@ -1,30 +1,30 @@
 return {
-	"stevearc/conform.nvim",
-	cmd = { "ConformInfo" },
-	keys = {
-		{
-			-- Customize or remove this keymap to your liking
-			"<leader>f",
-			function()
-				require("conform").format({ async = true, lsp_fallback = true })
-			end,
-			mode = "",
-			desc = "Format buffer",
-		},
-	},
-	-- Everything in opts will be passed to setup()
-	opts = {
-		-- Define your formatters
-		formatters_by_ft = {
-			lua = { "stylua" },
-			python = { "isort", "blue" },
-			javascript = { { "prettierd", "prettier" } },
-		},
-		-- Customize formatters
-		formatters = {
-			shfmt = {
-				prepend_args = { "-i", "2" },
-			},
-		},
-	},
+  "stevearc/conform.nvim",
+  cmd = { "ConformInfo" },
+  keys = {
+    {
+      -- Customize or remove this keymap to your liking
+      "<leader>f",
+      function()
+        require("conform").format({ async = true, lsp_fallback = true })
+      end,
+      mode = "",
+      desc = "Format buffer",
+    },
+  },
+  config = function()
+    require('conform').setup({
+      formatters_by_ft = {
+        lua = { "stylua" },
+        python = { "isort", "blue" },
+        javascript = { "prettierd", "prettier" },
+      },
+      -- Customize formatters
+      formatters = {
+        shfmt = {
+          prepend_args = { "-i", "2" },
+        },
+      },
+    })
+  end
 }
